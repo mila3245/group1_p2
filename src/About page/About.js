f258dfaedd840b804578d96b7c42b6b93d98af42
Kamila Swiader <kamila_swiader93@hotmail.com>
2023-06-06 11:24:48 +0100

    colour changes and about section

---
+++ src/About page/About.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+    return (
+        <section className="hero is-large is-info">
+  <div className="hero-body">
+    <p className="title">
+      Dialearn
+    </p>
+    <p className="subtitle">
+        Learning a new language, or traveling with minimal knowledge of the local language, can be a challenge. 
+        But with the right resources, you can transform that challenge into an adventure.
+        Learning new language or dialect has never been faster.
+        This app provide you with full control of the pace at which you learn and produces high-quality translations on time. 
+    </p>
+  </div>
+</section>
+
+    )
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Lango from "./Lango";
 import Footer from "./Footer/Footer";
-
+import translatepg from "./Translate/translatepg"
+import About from "./About page/About"
 
 const App=()=>{
     return (
     <main>
     <Lango/>
+    <About/>
     <Footer/>
     </main>
     )
